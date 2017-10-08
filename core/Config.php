ca12e508280e4a1313d553a3f7cb1c3cc9f2209c
gurujagru <dasvrgnut@yahoo.com>
2017-10-08 12:23:38 +0200

    Updating Router and Config class

---
+++ core/Config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 08-Oct-17
+ * Time: 11:26 AM
+ */
+
+namespace blog\core;
+
+
+class Config
+{
+    private $data;
+    private static $instance;
+
+    private function __construct()
+    {
+        $this->data = require('routes.php');
+    }
+    public static function getInstance()
+    {
+        if (self::$instance == null){
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+    public function getDataConfig()
+    {
+        return $this->data;
+    }
+}
